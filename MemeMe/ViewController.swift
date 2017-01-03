7b40c6012a78d8ab663d9ecae2f3b82416e3959e
张良 <blazers@zhangliangdeMacBook-Pro.local>
2017-01-03 22:46:16 +0800

    Second commit

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,141 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var topButtons: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Init textfiled config
+        initTextField([topTextField, bottomTextField])
+        hideTextField()
+        // detect devcice camera enable or not
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // subscribe keyboard
+        subscribeToKeyboardNotifications()
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    // MARK: -- Init TextFiled control functions
+    
+    func initTextField(_ textFields : [UITextField]) {
+        
+        let memeTextAttributes:[String:Any] = [
+            NSStrokeColorAttributeName: UIColor.black,
+            NSForegroundColorAttributeName: UIColor.white,
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName: -1.0]
+        
+        for textField in textFields {
+            textField.textAlignment = .center
+            textField.delegate = self
+            textField.defaultTextAttributes = memeTextAttributes
+        }
+    }
+    
+    func showTextField() {
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        topTextField.isHidden = false
+        bottomTextField.isHidden = false
+        topButtons.isHidden = false
+    }
+    
+    func hideTextField() {
+        topTextField.isHidden = true
+        bottomTextField.isHidden = true
+        topButtons.isHidden = true
+    }
+    
+    
+    // MARK: -- IBAction
+    
+    @IBAction func pickFromCamera(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickFromAlbum(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: -- Album picker delegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            showTextField()
+        } else if imagePickerView.image == nil {
+            hideTextField()
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+        if imagePickerView.image == nil {
+            hideTextField()
+        }
+    }
+    
+    
+    // MARK: -- TextField delegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: -- Keyboard observer
+    func keyboardWillShow(_ notification:Notification) {
+        print("== Keyboard Show ==")
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func keyboardWillHide(_ notification:Notification) {
+        print("== Keyboard Hide ==")
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
 }
 
