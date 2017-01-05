629058494e6822c893dd6d5f57a7576e3667eb49
Blazers <308802880@qq.com>
2017-01-05 13:07:04 +0800

    2.0

---
+++ MemeMe/SentMemesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SentMemesViewController.swift
+//  MemeMe
+//
+//  Created by blazers on 2017/1/5.
+//  Copyright © 2017年 张良. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
